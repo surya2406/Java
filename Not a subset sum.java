357562f792da9b5a624cefbafe366bc5189bb340
surya2406 <95280700+surya2406@users.noreply.github.com>
2024-10-27 08:48:13 +0530

    Create Not a subset sum.java

---
+++ Not a subset sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long findSmallest(int[] arr) {
+        // Your code goes here
+        int res=1;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>res) return res;
+            else
+            {
+                res+=arr[i];
+            }
+        }
+        return res;
+    }
+}
