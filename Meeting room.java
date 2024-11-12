e901e21d05cc291d3e98a3569d79ae9b792ba064
surya2406 <95280700+surya2406@users.noreply.github.com>
2024-11-12 12:51:42 +0530

    Create Meeting room.java

---
+++ Meeting room.java
@@ -0,0 +1,11 @@
+class Solution {
+    static boolean canAttend(int[][] arr) {
+        // Your code here
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i][0]<arr[i-1][1]) return false;
+        }
+        return true;
+    }
+}
