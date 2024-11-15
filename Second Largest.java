26f93efc7240044bd43a83ba90a984a538001226
surya2406 <95280700+surya2406@users.noreply.github.com>
2024-11-15 09:36:03 +0530

    Create Second Largest.java

---
+++ Second Largest.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // Code Here
+        Arrays.sort(arr);
+        int n=arr.length;
+        int largest=-1,secondLargest=-1;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>largest)
+            {
+                secondLargest=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]<largest && arr[i]>secondLargest)
+            {
+                secondLargest=arr[i];
+            }
+        }
+        return secondLargest;
+        
+    }
+}
